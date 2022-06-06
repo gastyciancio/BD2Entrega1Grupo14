e28df30321361eaf1d4f71b01eb68f2574323a83
Gaston Ciancio <gastonciancio4@gmail.com>
2022-06-06 19:15:37 -0300

    se saco un import sin usar

--- src/main/java/ar/edu/unlp/info/bd2/repositories/VaxRepository.java
+++ src/main/java/ar/edu/unlp/info/bd2/repositories/VaxRepository.java
@@ -6,7 +6,6 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import java.util.*;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.*;
 
